930bc6edbc4cefd096eee8468d20e1daa8d799e4
EC2 Default User <ec2-user@ip-172-31-19-230.us-east-2.compute.internal>
2019-05-13 08:03:10 +0000

    Added ACF PRO

--- themes/fictional-univeristy-theme/front-page.php
+++ themes/fictional-univeristy-theme/front-page.php
@@ -76,32 +76,23 @@
 </div>
 
 <div class="hero-slider">
-    <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg') ?>);">
+    
+    <?php 
+        $ms = get_field('mini_slider');
+        //var_dump($ms);
+        //die;
+    ?>
+    <?php foreach( $ms as $key=>$value): ?>
+    <?php //var_dump($value).'<br>'?>
+    <div class="hero-slider__slide" style="background-image: url(<?php echo $value['image']['url'] ?>);">
         <div class="hero-slider__interior container">
             <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">Free Transportation</h2>
-                <p class="t-center">All students have free unlimited bus fare.</p>
+                <h2 class="headline headline--medium t-center"><?php echo $value['title'] ?></h2>
+                <p class="t-center"><?php echo $value['subtitle'] ?></p>
                 <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
             </div>
         </div>
     </div>
-    <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg') ?>);">
-        <div class="hero-slider__interior container">
-            <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">An Apple a Day</h2>
-                <p class="t-center">Our dentistry program recommends eating apples.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-            </div>
-        </div>
-    </div>
-    <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg') ?>);">
-        <div class="hero-slider__interior container">
-            <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">Free Food</h2>
-                <p class="t-center">Fictional University offers lunch plans for those in need.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-            </div>
-        </div>
-    </div>
+    <?php endforeach; ?>
 </div>
 <?php get_footer(); ?>
